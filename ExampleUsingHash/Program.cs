05a325d85f7a3d7e369b2bb4e8c097379313c7e2
Wodson Correia <wodsonluiz@live.com>
2022-05-28 17:41:59 -0300

    initial commit

---
+++ ExampleUsingHash/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace ExampleUsingHash
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var pass = "teste";
+            var pass2 = "teste";
+
+            var passHash = GetHashString(pass);
+            var passHash2 = GetHashString(pass2);
+
+            Console.WriteLine("******* hash 1");
+            Console.WriteLine(passHash);
+            Console.WriteLine("******* hash 2");
+            Console.WriteLine(passHash2);
+        }
+
+        private static string GetHashString(string inputString)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (byte b in GetHash(inputString))
+                sb.Append(b.ToString("X2"));
+
+            return sb.ToString();
+        }
+
+        private static byte[] GetHash(string inputString)
+        {
+            using HashAlgorithm algorithm = SHA256.Create();
+            return algorithm.ComputeHash(Encoding.UTF8.GetBytes(inputString));
+        }
+    }
+}
